4688193d3903d6a181079838f3e71257e86510ff
Pallavi-singh-dev <thakurpallavisingh172005@gmail.com>
2026-03-22 18:18:36 +0530

    Creating Nexa

---
+++ server/lib/utils.js
@@ -0,0 +1,9 @@
+import jwt from "jsonwebtoken"
+
+// function tot gemerate a token from user
+
+export const generateToken = (userId) =>{
+ const token = jwt.sign({userId},process.env.JWT_SECRET);
+ return token;
+ 
+}
